883651645811056322c2fdc15e500fbfd563643a
Francesco Viccica <viccicafrancesco@gmail.com>
2025-05-11 17:17:30 +0200

    esercizio modificato

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let persona = {
     surname : 'Viccica',
     age : 30,
     saluta: function () {
-        console.log(`Ciao a tutti mi chiamo Francesco`);
+        // console.log(`Ciao a tutti mi chiamo Francesco`);
+        console.log(`Ciao a tutti sono ${this.name} ${this.surname} ed ho ${this.age} anni, piacere di conoscerti!`);
+        
         
     }
 
