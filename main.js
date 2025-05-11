20dea054a736b0a604181c5ded74a4db690dcc94
Francesco Viccica <viccicafrancesco@gmail.com>
2025-05-11 17:11:20 +0200

    esercizio svolto

---
+++ main.js
@@ -0,0 +1,12 @@
+let persona = {
+    name : 'Francesco',
+    surname : 'Viccica',
+    age : 30,
+    saluta: function () {
+        console.log(`Ciao a tutti mi chiamo Francesco`);
+        
+    }
+
+}
+
+persona.saluta()
